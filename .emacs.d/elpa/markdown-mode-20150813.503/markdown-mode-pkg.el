28f96dcc4eca2e2cff53edf2450d1fb9650e6081
jabbalaci <jabba.laci@gmail.com>
2015-09-16 14:24:53 +0200

    update

--- .emacs.d/elpa/markdown-mode-20150813.503/markdown-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "markdown-mode" "20150813.503" "Emacs Major mode for Markdown-formatted text files" (quote nil))
--- .emacs.d/elpa/rust-mode-20150823.1024/rust-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rust-mode" "20150823.1024" "A major emacs mode for editing Rust source code" (quote nil))
