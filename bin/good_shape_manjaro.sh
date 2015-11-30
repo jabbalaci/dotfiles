8f571d24502affb9fa24a6435c381283f2e8b936
Jabba Laci <jabba.laci@gmail.com>
2015-12-01 00:58:42 +0100

    update

--- bin/good_shape_manjaro.sh
+++ bin/good_shape_manjaro.sh
@@ -4,8 +4,10 @@
 #
 # good_shape_manjaro.sh
 
-sudo pacman-mirrors -g
-sudo pacman -Syy
-sudo pacman -Syu
-sudo pacman -R $(pacman -Qdtq)
+#sudo pacman-mirrors -g
+#sudo pacman -Syy
+#sudo pacman -Syu
+#sudo pacman -R $(pacman -Qdtq)
+
+yaourt -Syua
 sudo pacman -Scc
