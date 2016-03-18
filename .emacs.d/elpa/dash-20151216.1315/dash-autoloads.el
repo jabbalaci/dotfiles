8621ebc3cdfadd6db392a32b7bf13eab4dbb1a73
Jabba Laci <jabba.laci@gmail.com>
2016-03-18 20:07:53 +0100

    emacs packages updated

--- .emacs.d/elpa/dash-20151216.1315/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dash.el") (22156 60596 469061 205000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
